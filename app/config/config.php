8f4d533bb4efdbbdd01758df45c04d2a892f9d0e
stefanoschoonheimMBO <320791@student.mboutrecht.nl>
2023-09-07 13:15:46 +0200

    Changed MVC to be in laravel framework

--- app/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'db-rocambolesque');
-    define('URLROOT', 'http://Rocambolesque.nl'); //WIP URLROOT kan nog aangepast worden.
--- app/require.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once 'libraries/Core.php';
-    require_once 'libraries/BaseController.php';
-    require_once 'libraries/Database.php';
-    require_once 'config/config.php';
-
-$init = new Core();
