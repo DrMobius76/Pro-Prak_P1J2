31251705e28e37aecd5162a84499c65d7fcb1794
SimonPras <332959@student.mboutrecht.nl>
2023-09-21 14:02:09 +0200

    Homepage gemaakt
    Homepage gemaakt voor het project

---
+++ Homepage/Homepage.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-6">
+                <div class="fronttext">
+                    <div class="header">
+                        <div class="logo">
+                            <div class="form-group">
+                                <label for="Image"><a href="./Homepage.php"><img id="rocam-logo" src="./Rocambolesque-logo-DEF.png" alt="Image"></a></label>
+                                <div class="navbar">
+                                    <a href="./Homepage.php">Menu</a>
+                                    <a href="./Homepage.php">Make reservation</a>
+                                    <a href="./Homepage.php">About us</a>
+                                </div>
+                                <div class="login">
+                                    <a href="./Homepage.php">Login</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-6">
+                <div class="fronttext">
+                    <div class="form-group">
+                        <h3>Rocambolesque</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="homepageinfo">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-6">
+                    <div class="block">
+                        <div class="form-group">
+                            <label for="text">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-6">
+                    <div class="image">
+                        <div class="form-group">
+                            <label for="Image"><img src="https://www.tasteofhome.com/wp-content/uploads/2020/04/Chilis-3.jpg?resize=1024%2C683" alt="Image"></label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-6">
+                <div class="linkimages">
+                    <div class="form-group">
+                        <label for="Image"><a href="./Homepage.php"><img id="twitter-image" src="./Twitter-logo.png" alt="Image"></a></label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-6">
+                <div class="linkimages">
+                    <div class="form-group">
+                        <label for="Image"><a href="./Homepage.php"><img id="instagram-image" src="./instagram.png" alt="Image"></a></label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
