7c4072a51a9685237490d4171daf4e691c02c01c
vincent van de merwe <335479@student.mboutrecht.nl>
2023-09-29 14:21:10 +0200

    added column is_admin to the users table also added two rows of information, as in: name, email, password, is_admin, to the table users also changed the charset and collation to UTF8 instead of UTF8mb4 and also gave the database the engine InnoDB in de config/database.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -17,9 +18,25 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert([
+            [
+                'name' => 'angela van kok',
+                'email' => 'angela@gmail.com',
+                'password' => '1234',
+                'is_admin' => 0
+            ],
+            [
+                'name' => 'Robert de kip',
+                'email' => 'Robert.work@gmail.com',
+                'password' => '0987',
+                'is_admin' => 1
+            ]
+        ]);
     }
 
     /**
