92fda1d0cb5f590cf5b618fabc9298fc9cea9946
vincent van de merwe <335479@student.mboutrecht.nl>
2023-09-29 13:42:25 +0200

    added migration table reservation to the repository and gave the migration the information for all the columns in the table reservation

---
+++ database/migrations/2023_09_29_113328_create_reservation_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->id();
+            $table->string('days');
+            $table->date('date');
+            $table->string('time');
+            $table->string('table');
+            $table->string('child_chairs')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservation');
+    }
+};
