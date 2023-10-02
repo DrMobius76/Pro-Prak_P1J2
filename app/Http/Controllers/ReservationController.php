35405f550ce87108cd458156de3b742c91506ba9
vincent van de merwe <335479@student.mboutrecht.nl>
2023-10-02 13:40:31 +0200

    in the reservationcontroller added a function index with $information with information from the indexreservation function in the model, in the model reservation added a function indexreservation with all the information from the table reservation, in de folder view added a folder reservation with a index.blade.php, in de routes commented out the welcome page route and added a route to the reservation index

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reservation;
+use Illuminate\Http\Request;
+
+class ReservationController extends Controller
+{
+    public function index()
+    {
+        $information = Reservation::indexReservation();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/control-reservation', [ReservationController::class, 'index'])->name('reservation.index');
