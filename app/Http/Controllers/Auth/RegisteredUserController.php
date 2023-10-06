2fb3dfaccdd32069c1d66448e7ae9aa249aa8314
vincent van de merwe <335479@student.mboutrecht.nl>
2023-10-06 13:33:44 +0200

    removed the isAdmin from the RegisteredUserController because isAdmin is for later not for now

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,7 +39,6 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'isAdmin' => 0,
             'password' => Hash::make($request->password),
         ]);
 
