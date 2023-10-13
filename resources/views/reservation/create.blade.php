6af940683aeebeb4d2b91e089baf3401ea53d461
vincent van de merwe <335479@student.mboutrecht.nl>
2023-10-13 14:17:31 +0200

    added comments to the elements on the creat.blade.php view file to all the divs for every input field, for the form also for the @method and for the @csrf

--- resources/views/reservation/create.blade.php
+++ resources/views/reservation/create.blade.php
@@ -8,30 +8,33 @@
 </head>
 
 <body>
+    //* when the form is send the user gets send to the name of the route where the user needs to go to
     <form method="post" action="{{route('reservation.store')}}">
+        //* this is security for laravel it checks if the user is the actually person that makes a request
         @csrf
+        //* sends the form as in a post method
         @method('post')
-        <div>
+        <div>//* this div is for the day fields
             <label for="days">dag:</label>
             <input class="edit" type="text" name="days" placeholder="dagen">
         </div>
-        <div>
+        <div>//* this div is for the date fields
             <label for="date">Datum:</label>
             <input class="edit" type="date" name="date" placeholder="datum">
         </div>
-        <div>
+        <div>//* this is for the time fields on the page
             <label for="time">Tijd:</label>
             <input class="edit" type="text" name="time" placeholder="tijd">
         </div>
-        <div>
+        <div>//* this is for the table fields for on the page
             <label for="table">Tafel:</label>
             <input class="edit" type="text" name="table" placeholder="tafel">
         </div>
-        <div>
+        <div>//* this is for the child_chairs field on the page
             <label for="child_chairs">Kinderstoelen:</label>
             <input class="edit" type="text" name="child_chairs" placeholder="kinderstoelen">
         </div>
-        <div>
+        <div>//* when the users clicks on this input button then the form gets send
             <input type="submit" value="opslaan">
         </div>
     </form>
