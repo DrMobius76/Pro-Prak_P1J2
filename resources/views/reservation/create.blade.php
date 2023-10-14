f8164c8a2091c15cb40f92ab885922e3981e0572
vincent van de merwe <335479@student.mboutrecht.nl>
2023-10-14 15:06:28 +0200

    changed the comments from //* to <!-- --> because the comments where shown on the page because html has a different way of commenting then backend code, also removed comments from the view.blade.php foreach loop because it gave a error that the foreach loop has been malformed

--- resources/views/reservation/create.blade.php
+++ resources/views/reservation/create.blade.php
@@ -8,33 +8,39 @@
 </head>
 
 <body>
-    //* when the form is send the user gets send to the name of the route where the user needs to go to
+    <!-- when the form is send the user gets send to the name of the route where the user needs to go to -->
     <form method="post" action="{{route('reservation.store')}}">
-        //* this is security for laravel it checks if the user is the actually person that makes a request
+        <!-- this is security for laravel it checks if the user is the actually person that makes a request -->
         @csrf
-        //* sends the form as in a post method
+        <!-- sends the form as in a post method -->
         @method('post')
-        <div>//* this div is for the day fields
+        <!-- this div is for the day fields -->
+        <div>
             <label for="days">dag:</label>
             <input class="edit" type="text" name="days" placeholder="dagen">
         </div>
-        <div>//* this div is for the date fields
+        <!-- this div is for the date fields -->
+        <div>
             <label for="date">Datum:</label>
             <input class="edit" type="date" name="date" placeholder="datum">
         </div>
-        <div>//* this is for the time fields on the page
+        <!-- this is for the time fields on the page -->
+        <div>
             <label for="time">Tijd:</label>
             <input class="edit" type="text" name="time" placeholder="tijd">
         </div>
-        <div>//* this is for the table fields for on the page
+        <!-- this is for the table fields for on the page -->
+        <div>
             <label for="table">Tafel:</label>
             <input class="edit" type="text" name="table" placeholder="tafel">
         </div>
-        <div>//* this is for the child_chairs field on the page
+        <!-- this is for the child_chairs field on the page -->
+        <div>
             <label for="child_chairs">Kinderstoelen:</label>
             <input class="edit" type="text" name="child_chairs" placeholder="kinderstoelen">
         </div>
-        <div>//* when the users clicks on this input button then the form gets send
+        <!-- when the users clicks on this input button then the form gets send -->
+        <div>
             <input type="submit" value="opslaan">
         </div>
     </form>
--- resources/views/reservation/edit.blade.php
+++ resources/views/reservation/edit.blade.php
@@ -7,33 +7,40 @@
     <title>Document</title>
 </head>
 
-<body>//* sends the user to the name of the route for the page they need to go to with the information from the database
+<body>
+    <!-- sends the user to the name of the route for the page they need to go to with the information from the database -->
     <form method="post" action="{{route('reservation.update', ['info' => $info])}}">
-        //* this is security for laravel it checks if the user is the actually person that makes a request
+        <!-- this is security for laravel it checks if the user is the actually person that makes a request -->
         @csrf
-        //* sends the form as in a put method so it updates the information
+        <!-- sends the form as in a put method so it updates the information -->
         @method('put')
-        <div>//* this div is for the days field
+        <!-- this div is for the days field -->
+        <div>
             <label for="days">Dag:</label>
             <input class="edit" type="text" name="days" placeholder="dagen" value="{{$info->days}}">
         </div>
-        <div>//* this div is for the date fields
+        <!-- this div is for the date fields -->
+        <div>
             <label for="date">Datum:</label>
             <input class="edit" type="date" name="date" placeholder="datum" value="{{$info->date}}">
         </div>
-        <div>//* this is for the time input
+        <!-- this is for the time input -->
+        <div>
             <label for="time">Tijd:</label>
             <input class="edit" type="text" name="time" placeholder="tijd" value="{{$info->time}}">
         </div>
-        <div>//* this is for the table columns
+        <!-- this is for the table columns -->
+        <div>
             <label for="table">Tafel:</label>
             <input class="edit" type="text" name="table" placeholder="tafel" value="{{$info->table}}">
         </div>
-        <div>//* this is for the child_chairs
+        <!-- this is for the child_chairs -->
+        <div>
             <label for="child_chairs">Kinderstoelen:</label>
             <input class="edit" type="text" name="child_chairs" placeholder="kinderstoelen" value="{{$info->child_chairs}}">
         </div>
-        <div>//* when the users clicks on this input button the form is going to send the data
+        <!-- when the users clicks on this input button the form is going to send the data -->
+        <div>
             <input type="submit" value="Update">
         </div>
     </form>
