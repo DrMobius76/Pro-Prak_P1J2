034c67ca24af325670d73124edc17077e0fcf1d6
vincent van de merwe <335479@student.mboutrecht.nl>
2023-10-23 14:44:15 +0200

    i forgot to add web.php from the last commit

--- routes/web.php
+++ routes/web.php
@@ -18,24 +18,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//* this sends the user to the view page and adds the reservationView function to the page
-Route::get('/reservation/view', [ReservationController::class, 'reservationView'])->name('reservation.view');
-//* this sends the user to the create page and adds the reservationCreate function to the page
-Route::get('/reservation/create', [ReservationController::class, 'reservationCreate'])->name('reservation.create');
-//* this sends the user to the view page but makes the reservation and put it in the database and adds the reservationStore function to the page
-Route::post('/reservation/view', [ReservationController::class, 'reservationStore'])->name('reservation.store');
-//* this sends the user to the edit page and adds the reservationEdit function to the page
-Route::get('/reservation/{info}/edit', [ReservationController::class, 'reservationEdit'])->name('reservation.edit');
-//* this sends the user to the view page but updates the reservation and adds the reservationUpdate function to the page
-Route::put('/reservation/{info}/update', [ReservationController::class, 'reservationUpdate'])->name('reservation.update');
-//* this sends the user to the view page and deletes the reservation and adds the reservationDelete function to the page
-Route::get('/reservation/{info}/delete', [ReservationController::class, 'reservationDelete'])->name('reservation.delete');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    //* this sends the user to the view page and adds the reservationView function to the page
+    Route::get('/reservation/view', [ReservationController::class, 'reservationView'])->name('reservation.view');
+    //* this sends the user to the create page and adds the reservationCreate function to the page
+    Route::get('/reservation/create', [ReservationController::class, 'reservationCreate'])->name('reservation.create');
+    //* this sends the user to the view page but makes the reservation and put it in the database and adds the reservationStore function to the page
+    Route::post('/reservation/view', [ReservationController::class, 'reservationStore'])->name('reservation.store');
+    //* this sends the user to the edit page and adds the reservationEdit function to the page
+    Route::get('/reservation/{info}/edit', [ReservationController::class, 'reservationEdit'])->name('reservation.edit');
+    //* this sends the user to the view page but updates the reservation and adds the reservationUpdate function to the page
+    Route::put('/reservation/{info}/update', [ReservationController::class, 'reservationUpdate'])->name('reservation.update');
+    //* this sends the user to the view page and deletes the reservation and adds the reservationDelete function to the page
+    Route::get('/reservation/{info}/delete', [ReservationController::class, 'reservationDelete'])->name('reservation.delete');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
