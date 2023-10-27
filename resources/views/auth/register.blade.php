2d85798c88fd05d09cf7ea9af8f7ba33e748f4b4
stefanoschoonheimMBO <320791@student.mboutrecht.nl>
2023-10-27 13:34:23 +0200

    Revert "Merge branch 'development' into Account-Login"
    This reverts commit 98427d3be1d1fdf339f37db6def326feecd0819c, reversing
    changes made to 17f74371e20cc43936e2ba04850ee6c291868341.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,21 +16,21 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- adds is admin -->
-        <x-text-input hidden type="text" name="isAdmin" :value=0 />
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Wachtwoord')" />
+
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" placeholder="Password1234" required autocomplete="new-password" />
+
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Bevestig Wachtwoord')" />
+
             <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" placeholder="Password1234" required autocomplete="new-password" />
+
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
