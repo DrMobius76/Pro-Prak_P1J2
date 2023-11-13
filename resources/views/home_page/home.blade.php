40e189b4e14c36e096e57a67a78a7f2f0821a1f4
vincent van de merwe <335479@student.mboutrecht.nl>
2023-11-13 11:34:57 +0100

    changed in the homepage.blade.php the app.css path to homepage.css and in the vite.config.js i changed the path to homepage.css

--- resources/views/home_page/home.blade.php
+++ resources/views/home_page/home.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    @vite(['resources/scss/homepage.scss', 'resources/css/app.css'])
+    @vite(['resources/scss/homepage.scss', 'resources/css/homepage.css'])
 
 </head>
 
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/scss/homepage.scss', 'resources/js/app.js']
+            input: ['resources/css/homepage.css', 'resources/scss/homepage.scss', 'resources/js/app.js']
 
         }),
     ], refresh: true
