22b711282ff84506c08763a2dba2e56b924a52e0
vincent van de merwe <335479@student.mboutrecht.nl>
2023-11-30 12:32:34 +0100

    added comments to the homepage.php and homepage.scss also removed a styling of height 100% because the code wasn't using the 100% of that styling

--- resources/views/homepage/home.blade.php
+++ resources/views/homepage/home.blade.php
@@ -10,6 +10,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <!-- this loads the styling of the homepage -->
     @vite(['resources/scss/homepage.scss', 'resources/css/homepage.css'])
 
 
@@ -38,23 +39,33 @@
         </div>
     </div>
     <div>
+        <!-- this is the container for the homepage title -->
         <div class="rocambolesque-text" id="homepage-title">
+            <!-- title of the homepage -->
             <h1>Rocambolesque</h1>
         </div>
     </div>
+    <!-- this is the container box for the information of the homepage -->
     <div class="homepageinfo" id="info-container">
         <div>
+            <!-- this is the box for the information text -->
             <div class="block" id="info-box">
+                <!-- in this element you can put the text for the information box -->
                 <label for="text">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</label>
             </div>
         </div>
+        <!-- this is the box for the img of the restaurant -->
         <div>
+            <!-- this puts it next to the img element -->
             <label for="Image">
+                <!-- this is the restaurants image -->
                 <img id="restaurant-img" src="https://www.tasteofhome.com/wp-content/uploads/2020/04/Chilis-3.jpg?resize=1024%2C683" alt="Image">
             </label>
         </div>
     </div>
+    <!-- puts the footer in this box -->
     <div>
+        <!-- this includes the footer to the homepage -->
         @include('layouts.footer')
     </div>
 </body>
