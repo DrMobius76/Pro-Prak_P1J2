59bb8fd6832debcb4d41039b65b0096da09d4849
vincent van de merwe <335479@student.mboutrecht.nl>
2023-12-07 12:25:53 +0100

    added a controller for the pages that the content of the page can be changed, also made two models with protected columns in the models and added two migrations with the columns already in the migration files

---
+++ database/migrations/2023_12_06_122731_create_changeable_pages_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('changeable_pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('content_id');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('changeable_pages');
+    }
+};
