4b59dad35e255db461eaa655ef656615cfa694a0
vincent van de merwe <335479@student.mboutrecht.nl>
2023-12-14 12:26:53 +0100

    added 2 models to the controller so the code can call the models to create information and also added 4 functions to create a title and a image to the database but also make headers and content and send it to the database they are 2 create function and 2 store functions

--- app/Http/Controllers/ChangeablePagesController.php
+++ app/Http/Controllers/ChangeablePagesController.php
@@ -3,9 +3,45 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\changeablePages;
+use App\Models\changeablePagesContent;
 use Illuminate\Http\Request;
 
 class ChangeablePagesController extends Controller
 {
-    //
+    public function createOneAboutUs()
+    {
+        return view('changeable_pages/about_us.index');
+    }
+
+    public function storeOneAboutUs(Request $request)
+    {
+        // dd($request);
+        $validated = $request->validate([
+            'title' => 'required',
+            'image' => 'nullable',
+        ]);
+
+        changeablePages::create($validated);
+
+        return redirect(route('aboutUs.createContent'));
+    }
+
+    public function createTwoAboutUs()
+    {
+        return view('changeable_pages/about_us.createContent');
+    }
+
+    public function storeTwoAboutUs(Request $request)
+    {
+        // dd($request);
+        $validated = $request->validate([
+            'header' => 'nullable',
+            'content' => 'required',
+        ]);
+
+        changeablePagesContent::create($validated);
+
+        return redirect(route('aboutUs.index'));
+    }
 }
