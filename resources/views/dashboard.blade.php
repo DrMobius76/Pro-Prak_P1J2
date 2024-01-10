03852516270ce32db797b84266bbf89375455c02
vincent van de merwe <335479@student.mboutrecht.nl>
2024-01-10 14:07:32 +0100

    added a control system that checks if the user is a admin or not on the dashboard for the links for the homepage, reserveringen, menu and about us page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,16 +11,17 @@
 
     <div class="button-container">
         <!-- <div id="button-subcontainer"> -->
-            <div class="button-subcontainer bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Je bent ingelogd!") }}
-                </div>
+        <div class="button-subcontainer bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+                {{ __("Je bent ingelogd!") }}
             </div>
+        </div>
         <!-- </div> -->
     </div>
 
     <div class="button-container">
         <div class="button-subcontainer bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+            @if (Auth::user()->isAdmin == 1)
             <a href="#">
                 <div class="menu p-6 text-gray-900 dark:text-gray-100">
                     {{ __("Verander het menu") }}
@@ -31,6 +32,23 @@
                     {{ __("Bekijk & verander reserveringen") }}
                 </div>
             </a>
+            @endif
+        </div>
+    </div>
+    <div class="button-container">
+        <div class="button-subcontainer bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+            @if (Auth::user()->isAdmin == 1)
+            <a href="#">
+                <div class="menu p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Verander de homepage") }}
+                </div>
+            </a>
+            <a href="#">
+                <div class="menu p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Verander de over ons pagina") }}
+                </div>
+            </a>
+            @endif
         </div>
     </div>
 
